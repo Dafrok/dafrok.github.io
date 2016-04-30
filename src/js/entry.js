f17a36b96d9cb2437b364d765299996841510732
Dafrok <o.o@mug.dog>
2016-04-30 21:32:21 +0800

    fastclick

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import hljs from 'highlight.js'
+import attachFastClick from 'fastclick'
 import 'fetch-polyfill'
 import './router.js'
+attachFastClick(document.body)
 hljs.initHighlightingOnLoad()
