69ad6b65c966c5349b49c069380944566b12c880
Dafrok <o.o@mug.dog>
2016-05-02 14:41:24 +0800

    friends

--- src/vue/friends/index.vue
+++ src/vue/friends/index.vue
@@ -1,3 +1,21 @@
 <template lang="jade">
 section Friends
+    ul
+        li(v-for="friend in friends")
+            a(v-text="friend.name", :href="firend.link", target="_blank")
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      friends: [
+        {name: 'Gerhut', link: 'http://gerhut.me'},
+        {name: 'Sinux', link: 'http://sinux.me/'},
+        {name: '楼教主', link: 'http://www.52cik.com/'},
+        {name: '喵窝', link: 'http://hikarievo.me/'}
+      ]
+    }
+  }
+}
+</script>
