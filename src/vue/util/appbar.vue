db0df35ee8e786416a84c6d032bc28262651953c
Dafrok <o.o@mug.dog>
2016-05-09 12:06:10 +0800

    remove useless message

--- src/vue/util/appbar.vue
+++ src/vue/util/appbar.vue
@@ -146,7 +146,6 @@ export default {
     methods: {
         pass () {},
         toggleMenu () {
-            console.log('1111')
             BaseStore.actions.toggleMenu()
         },
         back () {
