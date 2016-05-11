65dc0fcfe7ce8285a8dbc14f0fd22b681b50891a
Dafrok <o.o@mug.dog>
2016-05-11 14:52:12 +0800

    prevent document overflow scrolling

--- src/js/entry.js
+++ src/js/entry.js
@@ -6,3 +6,7 @@ import 'fetch-polyfill'
 import './router.js'
 fastclick.attach(document.body)
 hljs.initHighlightingOnLoad()
+
+document.addEventListener('touchstart', function (e) {
+    if(e.target === document){e.preventDefault()}
+})
