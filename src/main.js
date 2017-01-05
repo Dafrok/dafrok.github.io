551d129525e3a10c89b6b6cb5c5eb0a436855450
Dafrok <o.o@mug.dog>
2017-01-05 16:47:22 +0800

    Use VueRouter

--- src/main.js
+++ src/main.js
@@ -2,13 +2,21 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VueRouter from 'vue-router'
 import App from './App'
+import routes from './routes.js'
 
 Vue.use(Vuex)
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   template: '<App/>',
+  router,
   components: { App }
 })
