a4fa3fbeab52c2ae8413593001f413c38b27dde7
Dafrok <o.o@mug.dog>
2017-01-07 17:01:19 +0800

    Add route map

--- src/routes.js
+++ src/routes.js
@@ -1,2 +1,25 @@
+import About from './components/about'
+import Blog from './components/Blog'
+import Friends from './components/Friends'
+
 export default [
+  {
+    path: '/',
+    redirect: '/about'
+  },
+  {
+    name: 'ABOUT',
+    path: '/about',
+    component: About
+  },
+  {
+    name: 'BLOG',
+    path: '/blog',
+    component: Blog
+  },
+  {
+    name: 'FRIENDS',
+    path: '/friends',
+    component: Friends
+  },
 ]
